51305d134af6c41b01942f029e5c7cc8243e4da1
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-03 08:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+const level = 1;
+while(true){
+	if(element.tagName == "HTML"){
+		break;
+	}
+	level++;
+	element = element.parentNode;
+}
+alert(`The level of the element is: ${level}`);
